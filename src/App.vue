e03618c1cd39fc232860a8d555338e562ca355a6
Antoine BENHAIM <abenhaim@veepee.com>
2024-11-20 16:46:22 +0100

    First init

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="app">
+    <VueDraggable ref="el" :animation="150" v-model="list">
+      <div v-for="item in list" :key="item.id" class="p-3 text-primary-emphasis bg-primary-subtle border border-primary-subtle rounded-3">
+        {{ item.name }}
+      </div>
+    </VueDraggable>
+  </div>
+</template>
+
+<style scoped lang="scss" src="./App.scss"></style>
+<script setup lang="ts">
+  import { ref } from 'vue'
+  import { VueDraggable } from 'vue-draggable-plus'
+  
+  import 'bootstrap/dist/css/bootstrap.css'
+  import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+  const list = ref([
+    {
+      name: 'Joao',
+      id: 1
+    },
+    {
+      name: 'Jean',
+      id: 2
+    },
+    {
+      name: 'Johanna',
+      id: 3
+    },
+    {
+      name: 'Juan',
+      id: 4
+    }
+  ])
+</script>
---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "root/assets/styles/main.scss";`
+      }
+    }
+  },
+  plugins: [
+    vue()
+  ],
+  resolve: {
+    alias: {
+      'root': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
